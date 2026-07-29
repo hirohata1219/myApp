9e762232fbeaa2ad5c65cd6e238aa61ccb5d8304
hiro <m.hirohata.gero1219@gmail.com>
2026-07-29 13:42:05 +0900

    追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit]
+  before_action :set_user, only: %i[ show edit update destroy ]
   skip_before_action :require_login, only: [:new, :create]
 
   def index
@@ -29,16 +29,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     # @post = @user.posts.last
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path, success: t('users.update.success')
     else
@@ -48,7 +45,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to root_path, success: t('users.destroy.success')
   end
